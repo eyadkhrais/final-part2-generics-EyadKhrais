01714412c1ae01cb0509248eeb82d20260296456
eyad <eyadkhrais@yahoo.com>
2017-05-10 20:41:11 -0400

    part2

---
+++ final-part2-generics-EyadKhrais/src/com/mycompany/myapp/section2/MyGenClass.java
@@ -0,0 +1,28 @@
+package com.mycompany.myapp.section2;
+
+public class MyGenClass<K, V> implements MyGenInterface<K, V>{
+
+	K key;
+	V value;
+	
+	
+	public MyGenClass(K _key, V _value){
+		key = _key;
+		value = _value;
+	}
+
+
+	@Override
+	public K getKey() {
+		// TODO Auto-generated method stub
+		return key;
+	}
+
+
+	@Override
+	public V getValue() {
+		// TODO Auto-generated method stub
+		return value;
+	}
+
+}
